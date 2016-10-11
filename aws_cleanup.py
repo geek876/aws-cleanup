47ad5b7060bde9b6cf203798fe59dbdc124f518c
Yasin Amadmia <Yasin.Amadmia@TravisPerkins.co.uk>
2016-10-11 14:44:38 +0100

    initial commit

--- aws_cleanup.py
+++ aws_cleanup.py
@@ -2,31 +2,92 @@ import boto.ec2
 import boto.vpc
 import os
 import sys
-import urllib2
 
-
-ACCESS_KEY = raw_input("Enter AWS Access Key: ")
-SECRET_KEY = raw_input("Enter AWS Secret Key: ")
-
-if not ACCESS_KEY:
-  sys.exit("ERROR: Key error. Exiting")
+REGIONS = ['us-west-1']
+#REGIONS = ['us-east-1','us-west-1','ap-northeast-1','ap-southeast-2','sa-east-1','ap-southeast-1','ap-northeast-2','us-west-2','ap-south-1','eu-central-1','eu-west-1']
 
 VPC=[]
+SUBNETS=[]
+ROUTE_TABLES=[]
+IG=[]
+ACL=[]
 
-#regions = ['us-east-1','us-west-1','ap-northeast-1','ap-southeast-2','sa-east-1','ap-southeast-1','ap-northeast-2','us-west-2','ap-south-1','eu-central-1','eu-west-1']
-regions = ['us-west-1']
 
-for region in regions:
-  print("Region is: "+region)
+def print_banner(message):
+  print("")
+  divider="*"*len(message)
+  print(divider)
+  print(message)
+  print(divider)
 
-  vpc = boto.vpc.connect_to_region(region_name=region,aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
-  print("Total VPCs in this region", len(vpc.get_all_vpcs()))
-  for vpc in vpc.get_all_vpcs():
-    print("Doing VPC", vpc.id, vpc.tags)
+def build_vpc_list(vpc_connection):
+  for vpc in vpc_connection.get_all_vpcs():
     if not (vpc.tags)['Name']:
       VPC.append(vpc.id)
 
+def build_subnet_list(vpc_connection):
+  subnets=vpc_connection.get_all_subnets(filters=[('vpcId',VPC)])
+  for subnet in subnets:
+    SUBNETS.append(subnet.id)
 
-for vpcs in VPC:
-  print(vpcs)
-  
+def build_routetable_list(vpc_connection):
+  route_tables=vpc_connection.get_all_route_tables(filters=[('vpc-id',VPC)])
+  for route_table in route_tables:
+    ROUTE_TABLES.append(route_table.id)
+
+def build_ig_list(vpc_connection):
+  igs=vpc_connection.get_all_internet_gateways(filters=[('attachment.vpc-id',VPC)])
+  for ig in igs:
+    IG.append(ig.id)
+
+def build_acl_list(vpc_connection):
+  acls=vpc_connection.get_all_network_acls(filters=[('association.subnet-id', SUBNETS)])
+  for acl in acls:
+    ACL.append(acl.id)
+
+def print_assets_to_delete():
+  print("")
+  print("The Following Assets Will be Deleted")
+  print("")
+  print_banner("VPCs")
+  for vpc in VPC: print(vpc)
+  print_banner("Subnets")
+  for subnet in SUBNETS: print(subnet)
+  print_banner("Route Tables")
+  for rt in ROUTE_TABLES: print(rt)
+  print_banner("Internet Gateways")
+  for ig in IG: print(ig)
+  print_banner("Network ACLs")
+  for acl in ACL: print(acl)
+
+
+def cleanup():
+  print_assets_to_delete()
+  print("")
+  answer=raw_input("Are you sure you want to delete these assets (y/n)?: ")
+  print(answer)
+
+
+if __name__ == "__main__":
+
+  ACCESS_KEY=raw_input("Enter AWS ACCESS KEY: ")
+  SECRET_KEY=raw_input("Enter AWS SECRET kEY: ")
+
+  if not ACCESS_KEY or not SECRET_KEY:
+    sys.exit("ERROR: Key error. Exiting")
+
+  for region in REGIONS:
+    print("Region is: "+region)
+
+    vpc_connection = boto.vpc.connect_to_region(region_name=region,aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+
+    build_vpc_list(vpc_connection)
+    build_subnet_list(vpc_connection)
+    build_routetable_list(vpc_connection)
+    build_ig_list(vpc_connection)
+    build_acl_list(vpc_connection)
+
+    cleanup()
+
+
+
